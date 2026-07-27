f02f5734fe7e8121f555ff9d3f82bd51cf5ca705
Héctor Mendoza <hmendoza@iplideres.com>
2026-07-27 17:18:38 -0600

    Carga inicial del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'pantallas/home_screen.dart';
+
+void main() {
+  runApp(const GastosApp());
+}
+
+class GastosApp extends StatelessWidget {
+  const GastosApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gastos App',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFFF6B6B),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF6B6B)),
+        useMaterial3: true,
+        // configuracion de la fuente por defecto
+        fontFamily: 'Roboto',
+
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
